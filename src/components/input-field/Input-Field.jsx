a66f47278b0a4002c0d4edf10fc9f7f4e7f42710
Jil Muriel <djibrilmuriel@gmail.com>
2021-01-25 01:48:24 +0800

    Add input field component

---
+++ src/components/input-field/Input-Field.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { fade, withStyles } from "@material-ui/core/styles";
+import { InputBase, FormControl, InputLabel } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const BootstrapInput = withStyles((theme) => ({
+  root: {
+    "label + &": {
+      color: "#3A3A3A",
+      marginTop: theme.spacing(3),
+    },
+  },
+  input: {
+    marginTop: "7px",
+    marginBottom: "25px",
+    borderRadius: 4,
+    position: "relative",
+    backgroundColor: theme.palette.common.white,
+    border: "2px solid #D1D1D1",
+    fontSize: 16,
+    width: "100%",
+    padding: "10px 12px",
+    transition: theme.transitions.create(["border-color", "box-shadow"]),
+    "&:focus": {
+      borderColor: "#4F75F1",
+    },
+  },
+}))(InputBase);
+
+const useStyles = makeStyles({
+  label: {
+    fontSize: "22px",
+    color: "#3A3A3A",
+  },
+});
+
+export const InputField = ({ ...props }) => {
+  const classes = BootstrapInput;
+  const labelClasses = useStyles();
+  return (
+    <FormControl className={classes.margin} fullWidth display="block">
+      <InputLabel shrink className={labelClasses.label} htmlFor={props.id}>
+        {props.label}
+      </InputLabel>
+      <BootstrapInput {...props} />
+    </FormControl>
+  );
+};
+
+export default InputField;
