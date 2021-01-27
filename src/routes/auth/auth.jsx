7b1eff73930e3e32356be63de09906371db3b6ad
Jil Muriel <djibrilmuriel@gmail.com>
2021-01-28 00:03:03 +0800

    Add media query

--- src/routes/auth/auth.jsx
+++ src/routes/auth/auth.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { Box, Grid, Typography } from "@material-ui/core";
 import InputField from "../../components/input-field/Input-Field";
 import { makeStyles } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+
 const useStyles = makeStyles({
   authLayout: {
     minHeight: "100vh",
@@ -25,18 +27,24 @@ const useStyles = makeStyles({
     background: "#2755EE",
   },
   forgotPassword: {
-    fontSize: '18px',
-  }
+    fontSize: "18px",
+  },
 });
+
 export const Auth = () => {
   const classes = useStyles();
 
   const [userName, setUserName] = useState("");
   const [passWord, setPassword] = useState("");
 
+  const matches = useMediaQuery("(min-width:600px)");
+
   return (
     <Grid container className={classes.authLayout}>
-      <Grid item xs="6" className={classes.leftWrapper}></Grid>
+      {matches ? (
+        <Grid item xs="6" className={classes.leftWrapper}></Grid>
+      ) : null}
+
       <Grid container item display="flex" alignItems="center" xs="6">
         <Box className={classes.login}>
           <Typography variant="h1" className={classes.title}>
