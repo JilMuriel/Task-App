d82bc33fb74646c4e59c1d0546f844a56eacb704
Jil Muriel <djibrilmuriel@gmail.com>
2021-02-09 15:17:24 +0800

    Added private route component

--- src/routes/main.jsx
+++ src/routes/main.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-
+import { AuthProvider } from "../contexts/AuthContext";
 import Auth from "./auth/auth";
 import Home from "./home/Home";
 import PrivateRoute from "./private-route";
 
 export const Main = () => {
   return (
-    <Switch>
-      <PrivateRoute exact path="/">
-        <Home />
-      </PrivateRoute>
-      <Route path="/auth">
-        <Auth />
-      </Route>
-    </Switch>
+    <AuthProvider>
+      <Switch>
+        <PrivateRoute exact path="/" component={Home} />
+        <Route path="/login" component={Auth} />
+      </Switch>
+    </AuthProvider>
   );
 };
 
